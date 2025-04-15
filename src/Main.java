d2dc20a0d0a0b384fe6fd43308343edba75a7602
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-04-15 21:28:43 +0200

    Inicializimi i vargut REVERSE_ALPHABET per karaktere te vlefshme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
 
     private static final String BASE32_ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
     private static final int[] REVERSE_ALPHABET=new int[128];
+
+    static {
+        Arrays.fill(REVERSE_ALPHABET,-1);
+        for(int i=0;i<BASE32_ALPHABET.length();i++){
+            REVERSE_ALPHABET[BASE32_ALPHABET.charAt(i)]=i;
+        }
+    }
 }
