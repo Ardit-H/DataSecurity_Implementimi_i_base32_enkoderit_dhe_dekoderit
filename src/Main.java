69e3de2e870e313f6b6137dd5facb87f90bf864a
Ardit-H <ardit.hyseni5@student.uni-pr.edu>
2025-04-16 11:44:00 +0200

    Funksioni per kodim ne Base32

--- src/Main.java
+++ src/Main.java
@@ -11,4 +11,17 @@ public class Main {
             REVERSE_ALPHABET[BASE32_ALPHABET.charAt(i)]=i;
         }
     }
+
+    public static String encode(byte[] input){
+        StringBuilder encoded=new StringBuilder();
+        int i=0;
+        int index=0;
+        int currByte;
+        int nextByte;
+        int digit;
+
+        while(i<input.length){
+            currByte=(input[i]>=0) ? input[i] : (input[i]+256);
+        }
+    }
 }
