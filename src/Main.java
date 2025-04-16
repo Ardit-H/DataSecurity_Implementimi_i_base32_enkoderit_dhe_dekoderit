97d09d0f6c04806927765a811f95e197c2ed92fe
ErionMeh <erionm462@gmail.com>
2025-04-16 22:17:40 +0200

    Enkodimi

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,33 @@ public class Main {
 
         while(i<input.length){
             currByte=(input[i]>=0) ? input[i] : (input[i]+256);
+
+            if (index > 3) {
+                if ((i + 1) < input.length) {
+                    nextByte = (input[i + 1] >= 0) ? input[i + 1] : (input[i + 1] + 256);
+                } else {
+                    nextByte = 0;
+                }
+
+                digit = currByte & (0xFF >> index);
+                index = (index + 5) % 8;
+                digit <<= index;
+                digit |= nextByte >> (8 - index);
+
+                i++;
+            } else {
+                digit = (currByte >> (8 - (index + 5))) & 0x1F;
+                index = (index + 5) % 8;
+                if (index == 0) i++;
+            }
+
+            encoded.append(BASE32_ALPHABET.charAt(digit));
         }
+
+        while (encoded.length() % 8 != 0) {
+            encoded.append("=");
+        }
+
+        return encoded.toString();
     }
 }
